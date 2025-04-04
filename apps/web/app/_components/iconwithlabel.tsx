4f7ebc0ad4abae5f0fd24e7d9bca9f8d93ab6946
mukmik <158996932+mukumikuu@users.noreply.github.com>
2025-04-04 19:25:21 +0700

    fix: add color and layout

--- apps/web/app/_components/iconwithlabel.tsx
+++ apps/web/app/_components/iconwithlabel.tsx
@@ -4,16 +4,17 @@ interface IconWithLabelProps {
   icon: LucideIcon;
   label: string;
   size?: number;
+  color?: string;
 }
 const IconWithLabel = ({
   icon: Icon,
   label,
   size = 24,
+  color = "black",
 }: IconWithLabelProps) => {
   return (
-    <div className="flex items-center gap-2">
-      <Icon size={size} />
-      {}
+    <div className="inline-flex items-center gap-2">
+      <Icon size={size} color={color} />
       <span className="text-sm font-thin">{label}</span>
     </div>
   );
