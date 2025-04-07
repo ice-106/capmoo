b37560c868f9d8e3e600739f1249e7af685153b0
Tung <krittapop.kms@gmail.com>
2025-04-08 00:06:29 +0700

    feat: landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,48 +1,83 @@
 "use client";
 
-import React, { useState } from "react";
-import Header from "./_components/header";
-import Footer from "./_components/footer";
-import TextBox from "./_components/textbox";
-import Dropdown from "./_components/dropdown";
-import ProfilePhoto from "./_components/profilephoto";
-import IconWithLabel from "./_components/iconwithlabel";
-// add
-import { MapPin } from "lucide-react";
-import SearchBar from "./_components/searchbar";
+import React, { useEffect } from "react";
+import { useAuth } from 'react-oidc-context';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
-  const [selected, setSelected] = useState<string[]>([]);
-  const [inputValue, setInputValue] = useState("Use case: prefilled names");
-  const [searchQuery, setSearchQuery] = useState("");
+  const auth = useAuth();
+  const router = useRouter();
+
+  const signOutRedirect = () => {
+    const clientId = "45pi75s2fqmpp08p51pdupv5jc";
+    const logoutUri = "http://localhost:3000/";
+    const cognitoDomain = "https://ap-southeast-1kabcq3yw4.auth.ap-southeast-1.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  useEffect(() => {
+    // Only redirect if the user is authenticated
+    if (auth.isAuthenticated) {
+      router.push("/discover");
+    }
+  }, [auth.isAuthenticated, router]);
 
   return (
-    <main className="font-poppins w-full">
-      <Header text="Page" />
-      <Footer />
-      This is a page
-      <TextBox
-        name="test"
-        placeholder="Enter text here"
-        value={inputValue}
-        errorMessage={inputValue === "" ? "Input can't be empty" : ""}
-        onChange={(value) => setInputValue(value)} // Update state on change
-      />
-      <Dropdown
-        selected={selected}
-        onSelect={setSelected}
-        defaultText="Select something bro"
-        options={["a", "b", "c", "d", "e"]}
-      />
-      <p className="mt-4">Current value: {inputValue}</p>
-      <button onClick={() => console.log(selected)}>log selected</button>
-      <ProfilePhoto allowEdit={true} />
-      <IconWithLabel icon={MapPin} label="Chulalongkorn University" size={24} />
-      <SearchBar 
-        onSearch={(value => console.log(value))}
-        placeholder="Search..."
-        enableDrawer={true}
-      />
+    <main className="font-poppins w-full relative pt-[240px]">
+      {/* Background circle and Logo */}
+      <div
+        className={`absolute top-[-512px] left-[50%] -translate-x-1/2 flex justify-center items-end h-[720px] w-[720px] pb-16 rounded-full bg-pumpkin-lemon`}
+      >
+        <Image
+          src="/images/Logo.png"
+          alt="Capmoo"
+          width={144}
+          height={144}
+          className="w-[144px] h-auto"
+        />
+      </div>
+
+      <div
+        className="flex flex-col gap-12"
+      >
+        {/* Title */}
+        <div
+          className="flex flex-col items-center gap-4 w-full"
+        >
+          <h1
+            className="text-5xl text-black"
+          >
+            Capmoo
+          </h1>
+          <span
+            className="font-semibold text-xs"
+          >Get started, enhance your travel</span>
+        </div>
+
+        {/* Buttons */}
+        <div
+          className="flex flex-col items-center gap-2 w-full"
+        >
+          {auth.isLoading ? (
+            <div>
+              Loading...
+            </div>
+          ) : (
+            <>
+              <button onClick={() => auth.signinRedirect()}>Login</button>
+              <button onClick={() => signOutRedirect()}>Sign out</button>
+            </>
+
+          )}
+
+          {auth.error && (
+            <div className="text-red text-md">
+              {(auth.error as Error).message}
+            </div>
+          )}
+        </div>
+      </div>
     </main>
   );
 }
