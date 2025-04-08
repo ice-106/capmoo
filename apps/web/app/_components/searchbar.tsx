ce053ca468437785f63ccd813927d3fc67f70de8
mildrrnt <woranita888@gmail.com>
2025-04-08 20:31:22 +0700

    fix: fix nested form error

--- apps/web/app/_components/searchbar.tsx
+++ apps/web/app/_components/searchbar.tsx
@@ -43,11 +43,6 @@ const SearchBar: React.FC<SearchBarProps> = ({
     if (onChange) onChange(newValue);
   };
 
-  const handleSubmit = (e?: React.FormEvent) => {
-    if (e) e.preventDefault();
-    onSearch({ searchTerm: searchValue }); // Pass basic search data
-  };
-
   const handleDrawerSubmit = (formValues?: SearchFormValues) => {
     // Pass the complete form values from drawer
     onSearch(formValues);
@@ -56,43 +51,26 @@ const SearchBar: React.FC<SearchBarProps> = ({
 
   return (
     <>
-      {/* Use a div instead of form when drawer is enabled */}
-      {enableDrawer ? (
-        <div 
-          className="relative cursor-pointer"
-          style={{ width }}
-          onClick={() => setIsDrawerOpen(true)}
-        >
-          <Search 
-            className="absolute left-2 top-1/2 -translate-y-1/2 text-darkgrey"
-            size={20}
-          />
-          <input
-            type="text"
-            placeholder={placeholder}
-            value={searchValue}
-            readOnly={true}
-            className={`${baseStyles} ${currentStyles} cursor-pointer`}
-          />
-        </div>
-      ) : (
-        // Only use form when drawer is disabled
-        <form onSubmit={handleSubmit} className="relative" style={{ width }}>
-          <Search 
-            className="absolute left-2 top-1/2 -translate-y-1/2 text-darkgrey"
-            size={20}
-          />
-          <input
-            type="text"
-            placeholder={placeholder}
-            value={searchValue}
-            className={`${baseStyles} ${currentStyles}`}
-            onChange={handleChange}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
-          />
-        </form>
-      )}
+      <div 
+        className="relative cursor-pointer"
+        style={{ width }}
+        onClick={() => enableDrawer && setIsDrawerOpen(true)}
+      >
+        <Search 
+          className="absolute left-2 top-1/2 -translate-y-1/2 text-darkgrey"
+          size={20}
+        />
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={searchValue}
+          readOnly={enableDrawer}
+          className={`${baseStyles} ${currentStyles} ${enableDrawer ? 'cursor-pointer' : ''}`}
+          onChange={enableDrawer ? undefined : handleChange}
+          onFocus={() => !enableDrawer && setIsFocused(true)}
+          onBlur={() => !enableDrawer && setIsFocused(false)}
+        />
+      </div>
 
       {enableDrawer && isDrawerOpen && (
         <SearchDrawer
