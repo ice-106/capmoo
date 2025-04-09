85be01da88315f2abbc62b62683ee9a8a0fda14e
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-04-09 14:07:33 +0700

    feat: callback loading screen

--- apps/web/app/(pages)/callback/page.tsx
+++ apps/web/app/(pages)/callback/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import { useAuth } from 'react-oidc-context';
 
 export default function CallbackPage() {
@@ -12,10 +13,22 @@ export default function CallbackPage() {
     if (!auth.isLoading && auth.isAuthenticated) {
       router.push('/');
     }
-  }, [auth.isLoading, auth.isAuthenticated]);
+  }, [auth.isLoading, auth.isAuthenticated, router]);
 
-  if (auth.isLoading) return <div>Completing sign-in...</div>;
+  const renderLoader = () => (
+    <div className="flex w-full h-full justify-center items-center">
+      <div className="w-[180px] h-[180px] rounded-2xl bg-pumpkin-lemon p-6">
+        <Image
+          src="/images/Logo.png"
+          width={144}
+          height={144}
+          alt="Capmoo"
+          className="w-full h-full"
+        />
+      </div>
+    </div>
+  );
+
   if (auth.error) return <div>Error: {auth.error.message}</div>;
-
-  return <div>Redirecting...</div>;
+  else return renderLoader();
 }
