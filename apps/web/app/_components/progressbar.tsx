6c7959fff890b1b97c731f6af359f8d9f400d207
Unfortunated <thanyathornwiengperm@gmail.com>
2025-04-11 00:22:36 +0700

    fix: added duration for color transition and change color to correspond to global color palette

--- apps/web/app/_components/progressbar.tsx
+++ apps/web/app/_components/progressbar.tsx
@@ -14,8 +14,8 @@ const Progressbar: React.FC<ProgressbarProps> = ({ totalSteps, currentStep }) =>
       className="flex items-center justify-between w-full px-4 py-2">
       {[...Array(totalSteps)].map((_, index) => (
         <div key = {index}
-        className = {`h-2 flex-1 mx-1 rounded-full transition-all duration- 
-            ${index <= currentStep - 1 ? 'bg-orange' : 'bg-gray-300'}`}/>
+        className = {`h-2 flex-1 mx-1 rounded-full transition-all duration-200
+            ${index <= currentStep - 1 ? 'bg-orange' : 'bg-grey'}`}/>
       ))}
     </div>
   );
