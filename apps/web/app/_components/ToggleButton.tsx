2db597a3c0c3995062b1aafcb8ccd3f7f0f04bbc
narakrit <149227923+narakrit@users.noreply.github.com>
2025-04-10 23:51:31 +0700

    Update ToggleButton.tsx

--- apps/web/app/_components/ToggleButton.tsx
+++ apps/web/app/_components/ToggleButton.tsx
@@ -1,23 +1,24 @@
 // components/ToggleButton.tsx
 import React, {useState} from 'react';
 
-const ToggleButton: React.FC = () => {
-  const [toggle, setToggle] = useState("en");
-  const [isChecked, setIsChecked] = useState(false);
-  const [toggleLabel, setToggleLabel] = useState("leftLabel");
+interface ToggleButtonProps {
+  toggle: string;
+  onToggleChange: (toggle: string) => void;
+}
+
+const ToggleButton: React.FC<ToggleButtonProps> = ({ toggle, onToggleChange }) => {
+  const isChecked = toggle === "th";
 
   const handleToggle = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const isChecked = event.target.checked;
-    setIsChecked(isChecked);
-    setToggle(isChecked ? "th" : "en");    
-    setToggleLabel(isChecked ? "rightLabel" : "leftLabel");
-  }
+    const newToggle = event.target.checked ? "th" : "en";
+    onToggleChange(newToggle);
+  };
 
   return (
       <label className="inline-flex items-center mb-5 cursor-pointer">
-      <span className={`me-3 text-sm font-medium ${isChecked ? 'text-gray-500' : 'text-orange-500'}`}>EN</span>
-      <label htmlFor={toggleLabel} />
-      <input type="checkbox" value="" className="sr-only peer" onChange={handleToggle}/>
+      <span className={`me-3 text-sm font-medium ${isChecked ? 'text-grey' : 'text-orange'}`}>EN</span>
+      <label htmlFor={toggle} />
+      <input type="checkbox" value="" className="sr-only peer" checked={isChecked} onChange={handleToggle}/>
       <div className="relative w-11 h-6 bg-slate-100 rounded-full transition-colors duration-300">
         <img
           src={isChecked ? 'https://flagcdn.com/20x15/th.png' : 'https://flagcdn.com/20x15/us.png'}
@@ -25,7 +26,7 @@ const ToggleButton: React.FC = () => {
           className={`w-5 h-5 absolute top-[2px] start-[2px] rounded-full transition-all ${isChecked ? 'translate-x-full rtl:-translate-x-full' : ''}`}
         />
       </div>
-      <span className={`ms-3 text-sm font-medium ${isChecked ? 'text-orange-500' : 'text-gray-500'}`}>TH</span>
+      <span className={`ms-3 text-sm font-medium ${isChecked ? 'text-orange' : 'text-grey'}`}>TH</span>
     </label>
   );
 };
