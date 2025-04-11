c55b249a21024eb4450d1fb9049d8e56a9af7832
mildrrnt <woranita888@gmail.com>
2025-04-12 00:57:22 +0700

    feat: read more button

--- apps/web/app/activity/_components/reviewCard.tsx
+++ apps/web/app/activity/_components/reviewCard.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import TextBtn from '../../_components/textBtn';
 
 interface ReviewCardProps {
   profileImgUrl: string;
@@ -20,14 +21,17 @@ const ReviewCard = ({profileImgUrl, userName, reviewText, reviewUrl}: ReviewCard
       className="w-full p-2 border border-lightgrey rounded-lg gap-y-4 cursor-pointer"
       onClick={handleReviewClick}
       >
-      <span className="flex gap-x-4 items-center text-xs font-semibold mb-1">
-        <Image
-        src={profileImgUrl}
-        alt="Profile Image"
-        width={24}
-        height={24}
-        className="rounded-full" />
-        {userName}
+      <span className="flex justify-between items-center font-semibold mb-1">
+        <div className="flex gap-x-4 items-center">
+          <Image
+          src={profileImgUrl}
+          alt="Profile Image"
+          width={24}
+          height={24}
+          className="rounded-full" />
+          {userName}
+        </div>
+        <TextBtn text="Read" onClick={handleReviewClick} />
       </span>
       <p className="line-clamp-2 overflow-hidden text-ellipsis">
         {reviewText}
