9a7e12b84c54730b399b7fdfc82e9b88e8b7e53e
mildrrnt <woranita888@gmail.com>
2025-04-14 21:34:29 +0700

    feat: survey context and provider

---
+++ apps/web/app/survey/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { SurveyProvider } from './SurveyContext';
+
+export default function SurveyLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SurveyProvider>
+      {children}
+    </SurveyProvider>
+  );
+}
