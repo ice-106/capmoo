f3d3802422df96d6acf5a04e0def727173ecd9ab
ebonian <tanadon.santisan@gmail.com>
2025-04-18 18:38:44 +0700

    feat(db): normalize activity category to another table

--- apps/api/model/activity.go
+++ apps/api/model/activity.go
@@ -5,8 +5,8 @@ import "time"
 type Activity struct {
 	Model
 
-	Name             string    `gorm:"not null"`
-	Category         string    `gorm:"not null"` // normalize to another table ?
+	Name string `gorm:"not null"`
+
 	Description      string    `gorm:"not null"`
 	StartDateTime    time.Time `gorm:"not null"`
 	EndDateTime      time.Time `gorm:"not null"`
@@ -14,11 +14,13 @@ type Activity struct {
 	RemainSlot       int       `gorm:"not null"`
 	MaxParticipation int       `gorm:"not null"`
 
-	Reviews    []Review `gorm:"foreignKey:ActivityId"`
+	CategoryId uint     `gorm:"not null"`
+	Category   Category `gorm:"foreignKey:CategoryId"`
 	HostId     uint     `gorm:"not null"`
 	Host       Host     `gorm:"foreignKey:HostId"`
 	LocationId uint     `gorm:"not null"`
 	Location   Location `gorm:"foreignKey:LocationId"`
+	Reviews    []Review `gorm:"foreignKey:ActivityId"`
 }
 
 func (Activity) TableName() string {
